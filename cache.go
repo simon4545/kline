c361e2c4ed29af65ff9fddfd90e7143002c4acfe
simon4545 <simon4545@126.com>
2026-05-16 20:53:53 +0800

    fw

--- cache.go
+++ cache.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/ledisdb/ledisdb/config"
 	"github.com/ledisdb/ledisdb/ledis"
@@ -11,16 +13,26 @@ import (
 
 // LedisCache 使用ledisdb实现的缓存
 type LedisCache struct {
+	mu    sync.RWMutex
 	ledis *ledis.Ledis
 	db    *ledis.DB
 }
 
 // NewLedisCache 创建新的缓存实例
 func NewLedisCache() *LedisCache {
+	cache, err := NewLedisCacheWithErr()
+	if err != nil {
+		log.Printf("Failed to initialize LedisCache: %v", err)
+		return &LedisCache{}
+	}
+	return cache
+}
+
+// NewLedisCacheWithErr 创建新的缓存实例并返回错误
+func NewLedisCacheWithErr() (*LedisCache, error) {
 	dataDir := "./cache_data"
 	if err := os.MkdirAll(dataDir, 0755); err != nil {
-		log.Printf("Failed to create cache data directory: %v", err)
-		return &LedisCache{}
+		return nil, fmt.Errorf("create cache data directory: %w", err)
 	}
 
 	cfg := config.NewConfigDefault()
@@ -28,78 +40,129 @@ func NewLedisCache() *LedisCache {
 
 	l, err := ledis.Open(cfg)
 	if err != nil {
-		log.Printf("Failed to open ledis: %v", err)
-		return &LedisCache{}
+		return nil, fmt.Errorf("open ledis: %w", err)
 	}
 
 	db, err := l.Select(0)
 	if err != nil {
-		log.Printf("Failed to select database: %v", err)
 		l.Close()
-		return &LedisCache{}
+		return nil, fmt.Errorf("select database: %w", err)
 	}
 
-	return &LedisCache{ledis: l, db: db}
+	return &LedisCache{ledis: l, db: db}, nil
 }
 
 // SetEx 设置键值对，过期时间以小时为单位
 func (c *LedisCache) SetEx(key string, value interface{}, hours int64) {
-	if c == nil || c.db == nil {
-		return
-	}
-
-	var valueBytes []byte
-	switch v := value.(type) {
-	case string:
-		valueBytes = []byte(v)
-	case []byte:
-		valueBytes = v
-	default:
-		valueBytes = []byte(fmt.Sprintf("%v", v))
-	}
-
-	if err := c.db.Set([]byte(key), valueBytes); err != nil {
+	if err := c.SetExWithErr(key, value, hours); err != nil {
 		log.Printf("Failed to set key %s: %v", key, err)
-		return
+	}
+}
+
+// SetExWithErr 设置键值对，过期时间以小时为单位并返回错误
+func (c *LedisCache) SetExWithErr(key string, value interface{}, hours int64) error {
+	if c == nil {
+		return errors.New("cache is nil")
+	}
+	if key == "" {
+		return errors.New("key is empty")
+	}
+	if hours <= 0 {
+		return fmt.Errorf("invalid expiration hours: %d", hours)
+	}
+
+	c.mu.RLock()
+	db := c.db
+	c.mu.RUnlock()
+	if db == nil {
+		return errors.New("cache db is nil")
+	}
+
+	valueBytes := normalizeValue(value)
+
+	if err := db.Set([]byte(key), valueBytes); err != nil {
+		return fmt.Errorf("set value: %w", err)
 	}
 
 	duration := hours * 3600
-	if _, err := c.db.Expire([]byte(key), duration); err != nil {
-		log.Printf("Failed to set expiration for key %s: %v", key, err)
+	if _, err := db.Expire([]byte(key), duration); err != nil {
+		return fmt.Errorf("set expiration: %w", err)
 	}
+	return nil
 }
 
 // Get 获取键对应的值
 func (c *LedisCache) Get(key string) (interface{}, bool) {
-	if c == nil || c.db == nil {
-		return nil, false
-	}
-
-	value, err := c.db.Get([]byte(key))
+	v, ok, err := c.GetString(key)
 	if err != nil {
 		log.Printf("Failed to get key %s: %v", key, err)
 		return nil, false
 	}
-	if value == nil {
+	if !ok {
 		return nil, false
 	}
+	return v, true
+}
 
-	ttl, err := c.db.TTL([]byte(key))
+// GetString 获取键对应的字符串值
+func (c *LedisCache) GetString(key string) (string, bool, error) {
+	if c == nil {
+		return "", false, errors.New("cache is nil")
+	}
+	if key == "" {
+		return "", false, errors.New("key is empty")
+	}
+
+	c.mu.RLock()
+	db := c.db
+	c.mu.RUnlock()
+	if db == nil {
+		return "", false, errors.New("cache db is nil")
+	}
+
+	value, err := db.Get([]byte(key))
 	if err != nil {
-		log.Printf("Failed to get TTL for key %s: %v", key, err)
-		return nil, false
+		return "", false, fmt.Errorf("get value: %w", err)
 	}
-	if ttl == -1 || ttl == -2 {
-		return nil, false
+	if value == nil {
+		return "", false, nil
 	}
 
-	return string(value), true
+	ttl, err := db.TTL([]byte(key))
+	if err != nil {
+		return "", false, fmt.Errorf("get ttl: %w", err)
+	}
+	if ttl <= 0 {
+		return "", false, nil
+	}
+
+	return string(value), true, nil
 }
 
 // Close 关闭缓存连接
 func (c *LedisCache) Close() error {
-	if c != nil && c.ledis != nil {
-		c.ledis.Close()
+	if c == nil {
+		return nil
 	}
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.ledis != nil {
+		c.ledis.Close()
+		c.ledis = nil
+	}
+	c.db = nil
 	return nil
+}
+
+func normalizeValue(value interface{}) []byte {
+	switch v := value.(type) {
+	case string:
+		return []byte(v)
+	case []byte:
+		return v
+	default:
+		return []byte(fmt.Sprintf("%v", v))
+	}
 }
